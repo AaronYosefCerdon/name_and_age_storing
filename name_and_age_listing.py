4ad8b0febb563585cb029e29014f9c5ffbc1305c
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-10-21 12:09:41 +0800

    Initial Pseudocode

---
+++ name_and_age_listing.py
@@ -0,0 +1,11 @@
+#Initialize a list to properly store data.
+
+#Define the valid conditions for inputing a name.
+
+#Define the valid conditions for inputing an age.
+
+#Start a loop that will ask the user to input a name and an age which will stop or continue if yes or no is replied.
+
+#After the loop is complete, identify the oldest person entered.
+
+#Print the name and age of the oldest person entered.
