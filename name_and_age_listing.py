3b36ab0e5a3b8073b5537678b55f9e9a8c696bbf
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-10-21 23:34:11 +0800

    Input name complete with error message

--- name_and_age_listing.py
+++ name_and_age_listing.py
@@ -12,25 +12,27 @@ def valid_ages(age):
      #Define the conditions for an age to be a positive integer greater than 0.
      return age.isdigit( ) and int(age) > 0
 #Start a loop that will ask the user to input a name and an age which will stop or continue if yes or no is replied.
-
+while True:
+	
 #Input a try variable to print an error message.
-
-#Ask the user to enter a name.
-
-#Validate the name input.
-
-#If the entry is valid, proceed. If not, ask the user to enter a name again.
-
+      try:
+      #Ask the user to enter a name.
+           name = input("Please enter a name: ")
+      #Validate the name input.
+           if not valid_names(name):
+             #If the entry is valid, proceed. If not, ask the user to enter a name again.
+              raise ValueError("The name you entered is invalid. Please enter a valid name.")
 #Ask the user to input an age.
-
+      
 #Validate the age input.
 
 #If the entry is valid, proceed. If not, ask the user to enter an age again.
 
 #Store the collected information in the list.
 
-#Input an except variable to print an error message.
-
+      #Input an except variable to print an error message.
+      except ValueError as x:
+               print(x)
 #Ask the user if they want to continue putting in names and ages.
 
 #Define a variable to identify the oldest age/s.
