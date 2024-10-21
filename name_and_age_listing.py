6ac2a37998a6a6a7de9ab50189014045489d83fb
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-10-22 00:44:30 +0800

    Storing profiles in the list, asking user yes or no to entering more profiles  and adding if statement to stop the loop.

--- name_and_age_listing.py
+++ name_and_age_listing.py
@@ -1,6 +1,5 @@
 #Initialize a list to properly store data.
-def main ( ):
-	profiles = [ ]
+profiles = [ ]
   
 #Define a function for inputing a name. 
 def valid_names(name):
@@ -28,13 +27,17 @@ while True:
            if not valid_ages(age):
              #If the entry is valid, proceed. If not, ask the user to enter an age again.
               raise ValueError("The age you entered is invalid. Please enter a valid age.")
-#Store the collected information in the list.
-
-      #Input an except variable to print an error message.
-      except ValueError as x:
-               print(x)
-#Ask the user if they want to continue putting in names and ages.
-
+      #Store the collected information in the list.
+           profiles.append((name, int(age)))
+      #Ask the user if they want to continue putting in names and ages.
+	   enter_another_profile = input("Do you want to input another profile? (Yes/No): ").strip( ).lower( )
+      #Enter a condition where answering no stops the loop.
+	   if enter_another_profile != "yes":
+                  break
+      #Input an except variable to print an error message.	   
+      except ValueError as incorrect:
+               print(incorrect)
+       
 #Define a variable to identify the oldest age/s.
 
 #Define a variable to identify the names corresponding to the ages.
