a18c4dfd02ac0fc8973284b8030f02c7683f91bf
AaronYosefCerdon <aaronyosefcerdon123@gmail.com>
2024-10-21 22:46:32 +0800

    Complete Pseudocode

--- name_and_age_listing.py
+++ name_and_age_listing.py
@@ -1,11 +1,34 @@
 #Initialize a list to properly store data.
 
-#Define the valid conditions for inputing a name.
+#Define a function for inputing a name. 
+  
+#Define the condition for a name to consist of only alphabet letters.
 
-#Define the valid conditions for inputing an age.
+#Define a function for inputing an age.
+
+#Define the conditions for an age to be a positive integer greater than 0.
 
 #Start a loop that will ask the user to input a name and an age which will stop or continue if yes or no is replied.
 
-#After the loop is complete, identify the oldest person entered.
+#Ask the user to enter a name.
+
+#Using the try and except commands, validate the name input.
+
+#If the entry is valid, proceed. If not, ask the user to enter a name again.
+
+#Ask the user to input an age.
+
+#Using the try and except commands, validate the age input.
+
+#If the entry is valid, proceed. If not, ask the user to enter an age again.
+
+#Store the collected information in the list.
+#Ask the user if they want to continue putting in names and ages.
+
+#Define a variable to identify the oldest age/s.
+
+#Define a variable to identify the names corresponding to the ages.
 
 #Print the name and age of the oldest person entered.
+
+#Use an if statement to go back to the defined list.
